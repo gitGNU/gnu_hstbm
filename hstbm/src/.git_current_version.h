82e0a51917fb2ebdf0c2005a1cb34b3fd926b2fd
behoffski <behoffski@grouse.com.au>
2015-02-11 02:33:02 +1030

    Add placeholder hstbm/src/.git_current_version.h, in case the build does not generate an updated file.

---
+++ hstbm/src/.git_current_version.h
@@ -0,0 +1,27 @@
+/* .git_current_version.h -- Define version string for use in C code.
+
+   Default version description, giving base version and a disclaimer.
+   This file is a fall-back in case Git isn't installed; if it is,
+   the build process will overwrite this file with information about
+   the current state of the source tree, each time the program is
+   built.  */
+
+/* Copyright (C) 2015 Free Software Foundation, Inc.
+   Copyright (C) 2015 Grouse Software
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc.,
+   51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA */
+
+#define GIT_CURRENT_VERSION "0.12 [source state uncertain]"
